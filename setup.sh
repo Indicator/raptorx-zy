873c59d5c9ac5ebe5c43da8bc40cccd4204f0190
Zhiyong Wang <zywang@ttic.edu>
2015-05-16 17:39:18 -0500

    Fix MatrixLib CMakeList.template Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for i in MatrixLib bioh5 pnn1v2 ; do 
-( cd $i ; mkdir build; cd build; cmake ../ ; make ; make install ; )
+install_path=$(pwd)
+#for i in MatrixLib bioh5 pnn1v2 ; do
+for i in MatrixLib  ; do
+    sed -s "s|INSTALL_DIR|${install_path}|" $i/CMakeLists.template > $i/CMakeLists.txt
+#for i in MatrixLib bioh5 pnn1v2 ; do 
+( cd $i ; mkdir -p build; cd build; cmake ../ ; make ; make install ; )
 done
 
