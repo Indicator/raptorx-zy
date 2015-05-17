94d7c0d0ac8b244b400422230a0e8b95d9368270
Zhiyong Wang <zywang@ttic.edu>
2015-05-17 17:39:14 -0500

    Add CMakeLists.txt for bioh5, and pnn1v2.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 install_path=$(pwd)
-#for i in MatrixLib bioh5 pnn1v2 ; do
-#for i in MatrixLib ; do
-#for i in  bioh5  ; do
-for i in  pnn1v2  ; do
+for i in MatrixLib bioh5 pnn1v2 ; do
     sed -s "s|INSTALL_DIR|${install_path}|" $i/CMakeLists.template > $i/CMakeLists.txt
-#for i in MatrixLib bioh5 pnn1v2 ; do 
 ( cd $i ; mkdir -p build; cd build; cmake ../ ; make ; make install ; )
 done
 
